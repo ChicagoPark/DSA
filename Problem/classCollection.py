586f44582e339c472012a105368fe40aaf36326d
ChicagoPark <pyiphj@naver.com>
2022-04-13 22:13:52 +0900

    update

--- Problem/classCollection.py
+++ Problem/classCollection.py
@@ -1,6 +1,6 @@
 from random import randint
 class Node:
-    def __init__(self,value = None):
+        def __init__(self,value = None):
         self.value = value
         self.next = None
         self.prev = None
@@ -47,3 +47,53 @@ class linkedList:
         for i in range(n):
             self.add(randint(min_value, max_value))
         return self
+    
+class Queue:
+    def __init__(self):
+        self.linkedList = linkedList()
+    
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' '.join(values)
+    
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.linkedList.head == None:
+            self.linkedList.head = newNode
+            self.linkedList.tail = newNode
+        else:
+            self.linkedList.tail.next = newNode
+            self.linkedList.tail = newNode
+    
+    def isEmpty(self):
+        if self.linkedList.head == None:
+            return True
+        else:
+            return False
+    
+    def dequeue(self):
+        if self.isEmpty() == True:
+            return "There is not any node in the Queue"
+        else:
+            tempNode = self.linkedList.head
+            if self.linkedList.head == self.linkedList.tail:
+                self.linkedList.head = None
+                self.linkedList.tail = None
+            else:    
+                self.linkedList.head = self.linkedList.head.next
+            return tempNode
+                
+    
+    def peek(self):
+        if self.isEmpty() == True:
+            return "There is not any node in the Queue"
+        else:
+            return self.linkedList.head.value
+        
+    def delete(self):
+        if self.isEmpty() == True:
+            return "There is not any node in the Queue"
+        else:
+            self.linkedList.head = None
+            self.linkedList.tail = None
+
